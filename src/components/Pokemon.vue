c73cf32d16035d791d9a0fae30c5fd6a8ada4234
Dominik Dolejší <bukefalus007@gmail.com>
2024-09-13 14:11:11 +0200

    added dynmic env api

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,7 +1,10 @@
 <script async setup lang="ts">
 const name = "Bulbasaur"
 
-const pokemon = await fetch("https://pokeappbackend-dev.railway.internal/pokemon")
+// const pokemon = await fetch("https://pokeappbackend-dev.up.railway.app:8000/pokemon")
+console.log("ENV")
+console.log(import.meta.env.BACKEND_API)
+const pokemon = await fetch(`${import.meta.env.BACKEND_API}/pokemon`)
 console.log(pokemon)
 const data = await pokemon.json()
 console.log(data)
